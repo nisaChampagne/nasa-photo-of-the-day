2eb9b7b774f10dd1122d3e5a90687c5c9dc41988
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-07-17 19:50:40 -0400

    some more dates

--- src/Components/NasaHolder.js
+++ src/Components/NasaHolder.js
@@ -3,47 +3,24 @@ import axios from 'axios';
 
 
 export default function NasaHolder() {
-  // const [mediaUrl, setMediaUrl] = useState([]);
-  // const [imgTitle, setImgTitle] = useState([]);
-  // const [imgDate, setImgDate] = useState([]);
-  // const [imgInfo, setImgInfo] = useState([]);
 
   const [data, setData] = useState({});
+  const [date, setDate] = useState('2019-01-01');
 
-  console.log('use effect man');
 
 
     useEffect(() => {
-        axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-01-01`)
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
         .then(res => {
           const data = res.data;
           setData(data);
-
-
-
-  //   /////URL
-  //         const mediaUrl = res.data.url;
-  //         setMediaUrl(mediaUrl);
-
-  //  ////TITLE
-  //         const imgTitle = res.data.title;
-  //         setImgTitle(imgTitle);
-
-  //   ////DATE
-  //         const imgDate = res.data.date;
-  //         setImgDate(imgDate);
-
-  //   ///EXPLANATION
-  //         const imgInfo= res.data.explanation;
-  //         setImgInfo(imgInfo);
-
-
-
         });
-    }, []);
-  
+    }, [date]);
+
     return (
       <div className="cardHolder">
+        <button onClick={() => setDate("2019-01-02")}>01/02/2019</button>
+        <button onClick={() => setDate("2019-01-03")}>01/03/2019</button>
         <h2>{data.title}</h2>
         <p className='nasa-date'>{data.date}</p>
         <img className='nasaImage' alt='Space'src={data.url} style={{ maxWidth: "400px" }} />
